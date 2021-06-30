dae9ae05a74bd9d10bda0064cca7e2e5b5639526
SandunikaFernando <sandunika20fernando@gmail.com>
2021-06-30 23:02:54 +0530

    Data Structure- Arrays

--- test1.R
+++ test1.R
@@ -194,3 +194,16 @@ class((combineV))
 
 list1=list(v1,v2,v3)
 list1
+
+
+#arrays----
+#Create two vectors of different length
+vector1<-c(5,9,3)
+vector2<- c(10,11,12,13,14,15)
+column.names <-c("COL1","COL2","COL3")
+row.names <- c("ROW1","ROW2","ROW3")
+matrix.names <- c("Matrix1","Matrix2")
+
+#take these two vectors as inputs to the array
+result <-array(c(vector1,vector2),dim=c(3,3,2),dimnames=list(row.names,column.names,matrix.names))
+print(result)
